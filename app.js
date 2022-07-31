b5ee5ac34fc039dff1f1f951ab55ae348affc2eb
devihall <devis0306@gmail.com>
2022-07-31 14:28:13 -0400

    print dynamic output

---
+++ app.js
@@ -0,0 +1,19 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
+const printProfileData = (profileDataArr) => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log("================");
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+  
+}
+
+
+printProfileData(profileDataArgs)
+
