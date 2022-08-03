bc0198aaf38c6c16eca99d107a6b2ea6c0fb3e18
devihall <devis0306@gmail.com>
2022-08-03 17:28:01 -0400

    interfaced with inquirer

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer 
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers))
 
-const [name, github] = profileDataArgs;
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
 
+// fs.writeFile("./index.html", pageHTML, err => {
+//   if (err) throw err;
 
-
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
-
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
