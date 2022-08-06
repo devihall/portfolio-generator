51884b8421cfb2d7dfda03be61e715bae1467daa
devihall <devis0306@gmail.com>
2022-08-06 17:07:05 -0400

    project wrap up

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const inquirer = require("inquirer");
 const generatePage = require("./src/page-template");
 
@@ -138,12 +138,19 @@ Add a New Project
 
 promptUser()
   .then(promptProject)
-  .then(portfolioData => {
-   const pageHTML = generatePage(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+  .then((portfolioData) => {
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
   });
